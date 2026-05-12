da80d0e54d9407a4b61664293b13c5c071b29be3
fifteens-dev <fifteens-dev@users.noreply.github.com>
2026-05-12 15:18:38 +0000

    deploy: 773f611d6f5be053eb7a7b450d54dfe443540e7a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4259439451" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2381335719" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
